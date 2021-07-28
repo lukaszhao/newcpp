2248f01f3548a9593730fa234fc9c9f2b52ea938
Lukas Zhao <lukaszhao@gmail.com>
2021-07-28 00:28:59 -0400

    add more features

--- newcpp.py
+++ newcpp.py
@@ -1,6 +1,7 @@
 #!python
 
 
+from os import name
 from typing import ForwardRef
 
 
@@ -11,6 +12,15 @@ def generateMemberVar(classname):
     if len(classname) > 1:
         memberVar = memberVar + classname[1:]
     return memberVar
+
+
+def generateOnTheFlyVar(classname):
+    onTheFlyVar = ""
+    if len(classname) > 0:
+        onTheFlyVar = onTheFlyVar + classname[0].lower()
+    if len(classname) > 1:
+        onTheFlyVar = onTheFlyVar + classname[1:]
+    return onTheFlyVar
 
 
 FILEDOXYGEN = """/**
@@ -87,18 +97,16 @@ def generateInterfaceClassname(classname):
 def includeInterfaceHeader(namespace, classname):
     interfaceclassname = generateInterfaceClassname(classname)
     filenamepre = generateFilenamePre(namespace, interfaceclassname)
-    return "#include <" + filenamepre + ".h>\n"
+    return "#include <" + filenamepre + ".h>\n\n"
 
 def generateIncludeFiles(namespace, classname, needInterface, needPimpl):
     includeBlock = ""
     if needInterface:
         includeBlock = includeBlock + includeInterfaceHeader(namespace, classname)
     else:
-        includeBlock = includeBlock + "#include <string>\n"
+        includeBlock = includeBlock + "#include <string>\n\n"
     if needPimpl:
-        includeBlock = includeBlock + "#include <memory>\n"
-    if len(includeBlock) != 0:
-        includeBlock = includeBlock + "\n"
+        includeBlock = includeBlock + "#include <memory>\n\n"
     return includeBlock
 
 def generateNamespaceBegin(namespace):
@@ -123,7 +131,7 @@ def generateImpClassname(classname):
 
 
 DECLARE_IMP = """private:
-    std::shared_ptr<$$impclassname$$> $$impMemberVar$$;
+    std::unique_ptr<$$impclassname$$> $$impMemberVar$$;
 
 """
 
@@ -195,10 +203,161 @@ def generateHeaderFile(namespace, classname, classbrief, authorinfo, date, needI
     content = content + generateDeclareCtorDtor(classname)
     content = content + generateDummyFunctionVersion(needInterface)
 
-
     content = content + generateCloseDeclareClass()
     content = content + generateNamespaceEnd(namespace)
     content = content + generateIncludeGuardEnd(filenamepre)
 
+    return content
+
+
+def generateIncludeFilesForCpp(namespace, classname, needPimpl):
+    filenamepre = generateFilenamePre(namespace, classname)
+    h_filename = filenamepre + ".h"
+    includes = "#include <" + h_filename + ">\n\n"
+    if needPimpl:
+        impClassName = generateImpClassname(classname)
+        impFilePre = generateFilenamePre(namespace, impClassName)
+        impFileName = impFilePre + ".h"
+        includes = includes + "#include <" + impFileName + ">\n\n"
+    return includes
+        
+
+def generateDefineCtor(classname, needPimpl):
+    defineCtor = classname + "::" + classname + "()\n{\n"
+    if needPimpl:
+        impClassName = generateImpClassname(classname)
+        impVar = generateMemberVar(impClassName)
+        defineCtor = defineCtor + "    // inject dependencies into " + impVar + "\n"
+        defineCtor = defineCtor + "    " + impVar + " = std::make_unique<" + impClassName + ">();\n"
+    defineCtor = defineCtor + "}\n\n"
+    return defineCtor
+
+
+def generateDefineDtor(classname):
+    defineDtor = classname + "::~" + classname + "()\n{\n}\n\n"
+    return defineDtor
+
+
+def generateDefineFunctionVersion(classname, needPimpl):
+    defineFunctionVersion = "std::string " + classname + "::version() const\n{\n"
+    if needPimpl:
+        impClassName = generateImpClassname(classname)
+        impVar = generateMemberVar(impClassName)
+        defineFunctionVersion = defineFunctionVersion + "    return " + impVar + "->version();\n"
+    else:
+        defineFunctionVersion = defineFunctionVersion + "    return \"0.0.1\";\n"
+    defineFunctionVersion = defineFunctionVersion + "}\n\n"
+    return defineFunctionVersion
+
+
+def generateCppFile(namespace, classname, classbrief, authorinfo, date, needPimpl):
+    filenamepre = generateFilenamePre(namespace, classname)
+    filename = filenamepre + ".cpp"
+    content = generateFileDoxygen(filename, classbrief, authorinfo, date)
+    content = content + generateIncludeFilesForCpp(namespace, classname, needPimpl)
+    content = content + generateNamespaceBegin(namespace)
+    content = content + generateClassHeadCommentBlock(classname, isHeader=False)
+    content = content + generateDefineCtor(classname, needPimpl)
+    content = content + generateDefineDtor(classname)
+    content = content + generateDefineFunctionVersion(classname, needPimpl)
+
+    content = content + generateNamespaceEnd(namespace)
+    return content
+
+
+UNIT_TEST_CONTENT = """#include <gtest/gtest.h>
+
+// Subject Under Test (SUT)
+#include <$$sut_header_file$$>
+
+using namespace $$namespace$$;
+using namespace ::testing;
+
+class Test$$SutClassName$$ : public Test {
+protected:
+    void SetUp() {}
+    void TearDown() {}
+};
+
+TEST_F(Test$$SutClassName$$, test_ctor_and_dtor)
+{
+    EXPECT_NO_FATAL_ERROR($$SutClassName$$ $$on_the_fly_sut_var$$;);
+}
+
+TEST_F(Test$$SutClassName$$, test_version)
+{
+    // Given
+    $$SutClassName$$ $$on_the_fly_sut_var$$;
+
+    // When
+    auto result = $$on_the_fly_sut_var$$.vesion();
+
+    // Then
+    EXPECT_EQ(result, "0.0.1");
+}
+"""
+
+def generateUnitTest(namespace, classname, needPimpl):
+    sutClassName = classname
+    if needPimpl:
+        sutClassName = generateImpClassname(classname)
+    filenamePre = generateFilenamePre(namespace, sutClassName)
+    unitTestFileName = filenamePre + ".t.cpp"
+    sut_header_file = generateFilenamePre(namespace, sutClassName) + ".h"
+    on_the_fly_sut_var = generateOnTheFlyVar(sutClassName)
+    content = UNIT_TEST_CONTENT.replace("$$sut_header_file$$", sut_header_file)
+    content = content.replace("$$namespace$$", namespace)
+    content = content.replace("$$SutClassName$$", sutClassName)
+    content = content.replace("$$on_the_fly_sut_var$$", on_the_fly_sut_var)
 
     return content
+        
+
+def generateImpHeaderFile(namespace, classname, authorinfo, date):
+    impClassname = generateImpClassname(classname)
+    classbrief = "Implementation of " + classname
+    content = generateHeaderFile(namespace, impClassname, classbrief, authorinfo, date, needInterface=False, needPimpl=False)
+    return content
+
+
+def generateImpCppFile(namespace, classname, authorinfo, date):
+    impClassname = generateImpClassname(classname)
+    classbrief = "Implementation of " + classname
+    content = generateCppFile(namespace, impClassname, classbrief, authorinfo, date, needPimpl=False)
+    return content
+
+
+def generateMockClassName(classname):
+    return classname + "Mock"
+
+
+MOCK_INCLUDE_FILES= """#include <gmock/gmock.h>
+
+#include <$$interface_header_file$$>
+
+"""
+
+MOCK_CLASS_DECLARE = """
+class $$mockClassName$$ : public $$interfaceClassName$$ {
+public:
+    MOCK_CONST_METHOD0(version, std::string());
+};
+
+"""
+
+def generateMockHeaderFile(namespace, classname):
+    mockClassName = generateMockClassName(classname)
+    interfaceClassName = generateInterfaceClassname(classname)
+    interfaceHeaderFile = generateFilenamePre(namespace, interfaceClassName) + ".h"
+    mockFileNamePre = generateFilenamePre(namespace, mockClassName)
+    content = generateIncludeGuardBegin(mockFileNamePre)
+    content = content + MOCK_INCLUDE_FILES.replace("$$interface_header_file$$", interfaceHeaderFile)
+    content = content + generateClassHeadCommentBlock(mockClassName, isHeader=True)
+    content = content + MOCK_CLASS_DECLARE
+    content = content.replace("$$mockClassName$$", mockClassName)
+    content = content.replace("$$interfaceClassName$$", interfaceClassName)
+    content = content + generateIncludeGuardEnd(mockFileNamePre)
+    return content
+
+
+
--- newcpp.t.py
+++ newcpp.t.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.case import expectedFailure
 import newcpp
 
+"""
 class TestGenerateFunctions(unittest.TestCase):
     def test_generateHeaderFile(self):
         namespace = "rollingstone"
@@ -11,11 +12,57 @@ class TestGenerateFunctions(unittest.TestCase):
         classbrief = "A dog that barks"
         authorinfo = "Mariah Carey <mariahcarey@gmail.com>"
         date = "May 1st, 2021"
-        needInterface = False
+        needInterface = True
         needPimpl = True
         content = newcpp.generateHeaderFile(namespace, classname, classbrief, authorinfo, date, needInterface, needPimpl)
         print(content)
-    
+
+class TestGenerateCppFile(unittest.TestCase):
+    def test_generateCppFile(self):
+        namespace = "rollingstone"
+        classname = "Dog"
+        classbrief = "A dog that barks"
+        authorinfo = "Mariah Carey <mariahcarey@gmail.com>"
+        date = "May 1st, 2021"
+        needPimpl = True
+        content = newcpp.generateCppFile(namespace, classname, classbrief, authorinfo, date, needPimpl)
+        print(content)
+
+class TestGenerateUnitTest(unittest.TestCase):
+    def test_generateUnitTest(self):
+        namespace = "rollingstone"
+        classname = "Dog"
+        needPimpl = True
+        content = newcpp.generateUnitTest(namespace, classname, needPimpl)
+        print(content)
+
+class TestGenerateImpHeaderFile(unittest.TestCase):
+    def test_generateImpHeaderFile(self):
+        namespace = "rollingstone"
+        classname = "Dog"
+        classbrief = "A dog that barks"
+        authorinfo = "Mariah Carey <mariahcarey@gmail.com>"
+        date = "May 1st, 2021"
+        content = newcpp.generateImpHeaderFile(namespace, classname, authorinfo, date)
+        print(content)
+
+class TestGenerateImpCppFile(unittest.TestCase):
+    def test_generateImpCppFile(self):
+        namespace = "rollingstone"
+        classname = "Dog"
+        classbrief = "A dog that barks"
+        authorinfo = "Mariah Carey <mariahcarey@gmail.com>"
+        date = "May 1st, 2021"
+        content = newcpp.generateImpCppFile(namespace, classname, authorinfo, date)
+        print(content)
+"""
+
+class TestGenerateMockHeaderFile(unittest.TestCase):
+    def test_generateMockHeaderFile(self):
+        namespace = "rollingstone"
+        classname = "Dog"
+        content = newcpp.generateMockHeaderFile(namespace, classname)
+        print(content)
 
 if __name__ == '__main__':
     unittest.main()
